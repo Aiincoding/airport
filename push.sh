630f983deb296927b6098052f28ad79f5c717267
admin <admin@test.com>
2023-08-24 18:21:35 +0800

    YW-20230824: auto add commit

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WORK_DIR="$(cd "$(dirname $0)" && pwd)"
-cd $WORK_DIR || exit 1
-
-git add .
-git commit -m "YW-$(date +%Y%m%d): auto add commit"
-git push
-git push origin main:release
